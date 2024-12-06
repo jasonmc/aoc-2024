ee31e74eff6f815fd985b22bb4b6e1c93b3f3629
Jason McCandless <me@jasonmc.net>
2024-12-06 11:23:10 -0500

    Add fparsec.

---
+++ deps.nix
@@ -0,0 +1,3 @@
+{ fetchNuGet }: [
+  (fetchNuGet { pname = "FParsec"; version = "1.1.1"; hash = "sha256-BFTUFsdUDtPf3Y7YYsIHGnR3SykVeE6MAN3NRHv+Qwc="; })
+]
--- flake.nix
+++ flake.nix
@@ -1,13 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nuget-packageslock2nix = {
-      url = "github:mdarocha/nuget-packageslock2nix/main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { nixpkgs, nuget-packageslock2nix, ... }: {
+  outputs = { nixpkgs, ... }: {
     packages.aarch64-darwin.default =
       let
         pkgs = import nixpkgs { system = "aarch64-darwin"; };
@@ -18,13 +14,7 @@
         dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
         dotnet-runtime = pkgs.dotnetCorePackages.runtime_9_0;
         src = ./.;
-        nugetDeps = nuget-packageslock2nix.lib {
-          system = "aarch64-darwin";
-          name = "aoc-2024";
-          lockfiles = [
-            ./packages.lock.json
-          ];
-        };
+        nugetDeps = ./deps.nix;
       };
 
       formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
@@ -36,6 +26,7 @@
         buildInputs = with pkgs; [
           dotnetCorePackages.sdk_9_0
           fantomas
+          nuget-to-nix
         ];
       };
   };
